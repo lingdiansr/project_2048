100e81928ba2e0f6c1611e0dbf3b06d150ff9899
lingdianshiren <2077358365@qq.com>
2023-03-12 10:14:10 +0800

    master

--- main.c
+++ main.c
@@ -7,7 +7,8 @@
 #include <stdlib.h>
 void random_num()
 {
-    srand((unsigned)time(NULL));
+    time_t t;
+    srand((unsigned)time(&t));
     for (int i = 0; i < 10; i++)
     {
         printf("%d\n", (rand() % 2 + 1) * 2);
