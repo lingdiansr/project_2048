de7f5640d11b95744808fd4a852e33d04c44b515
lingdianshiren <2077358365@qq.com>
2023-03-12 10:40:45 +0800

    master

--- main.c
+++ main.c
@@ -5,10 +5,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-void random_num()
+void random_num()   //随机生成2和4
 {
     time_t t;
-    srand((unsigned)time(&t));
+    srand((unsigned)time(&t));  //随机种子
     for (int i = 0; i < 10; i++)
     {
         printf("%d\n", (rand() % 2 + 1) * 2);
